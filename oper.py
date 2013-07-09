b035a823707e58155355056bc3e757754129b7e4
Matt Fenwick <mfenwick100@gmail.com>
2013-07-09 09:05:32 -0400

    remove old version

--- oper.py
+++ oper.py
@@ -7,8 +7,7 @@ precs = {
     '^': 70
 }
 
-# 3 + 4 * 2 + 5
-# ((3 + (4 * 2)) + 5)
+
 def expr(f, ts1, power):
     if len(ts1) == 0:
         raise ValueError('bad input! empty')
@@ -27,31 +26,6 @@ def expr(f, ts1, power):
         right, ts3 = expr(lambda x: x, ts2, precs[op])
         return ({'op': op, 'left': left, 'right': right}, ts3)
 
-
-def expr2(ts1, power):
-    if len(ts1) == 1:
-        return (ts1[0], [])
-    elif len(ts1) == 0:
-        raise ValueError('bad input!  need args separated by operators')
-    fst, op, ts2 = ts1[0], ts1[1], ts1[2:]
-    if precs[op] > power:
-        right, ts3 = expr2(ts2, precs[op])
-        return ({'op': op,
-                 'left': fst,
-                 'right': right},
-                ts3)
-    else: # low precedence operator --> already found left arg, now find right
-        # return (fst, ts1[1:])
-        return rightSide2(fst, ts1[1:])
-        
-
-def rightSide2(left, ts1):
-    power = precs[ts1[0]]
-    right, ts2 = expr2(ts1[1:], power)
-    return ({'op': ts1[0],
-             'left': left,
-             'right': right},
-            ts2)
 
 def run(xs):
     tree, rest = expr(lambda x: x, xs, 8000)
