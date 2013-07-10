0647cb580512dc8117fdab70ae663e2fe8b92c1b
Matt Fenwick <mfenwick100@gmail.com>
2013-07-10 10:39:16 -0400

    start using power stack

--- oper.py
+++ oper.py
@@ -19,28 +19,28 @@ def result(op, l, r):
     return val
 
 
-def expr(f, ts1, power):
+def expr(f, ts1, powers):
     if len(ts1) == 0:
         raise ValueError('bad input! empty')
     elif len(ts1) == 1:
-        return (f(ts1[0]), [])
+        return f(ts1[0])
     fst, op, ts2 = ts1[0], ts1[1], ts1[2:]
-    print 'powers: ', precs[op], power
-    # keep parsing this expression until the precedence < power
-    if precs[op] > power:                       # ignore ties
+    print 'powers: ', precs[op], powers
+    # `fst` is the entire lhs of `op`; need to find `op`s rhs and remember the previous power(s) for later
+    if precs[op] > powers[0]:                       # ignore ties
         print ' first branch'
-        return expr(lambda x: f(result(op, fst, x)), ts2, precs[op])
-    else: # find right arg
+        return expr(lambda x: f(result(op, fst, x)), ts2, [precs[op]] + powers)
+    else: # `fst` **ends** the lhs of `op`; now we need to find `op`s rhs
+        # this means we can ditch the top power b/c its operator is done
         print '  second branch'
-        return expr(lambda x: result(op, f(fst), x), ts2, precs[op])
+        return expr(lambda x: result(op, f(fst), x), ts2, [precs[op]] + powers[1:])
 
 
 def start(xs):
     fst, op, ys = xs[0], xs[1], xs[2:]
-    return expr(lambda x: result(op, fst, x), ys, precs[op])
+    return expr(lambda x: result(op, fst, x), ys, [precs[op]])
 
 
 def run(xs):
-    tree, rest = start(xs)
+    tree = start(xs)
     print json.dumps(tree, indent=4)
-    print "rest: ", rest
