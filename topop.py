7878ce964441c62da3c165032c7d87086e23b4e8
Matt Fenwick <mfenwick100@gmail.com>
2013-07-11 13:58:18 -0400

    add case for right-associativity

--- topop.py
+++ topop.py
@@ -1,15 +1,22 @@
-import json
-
 
 def node(op, l, r):
     return {'op': op, 'left': l, 'right': r}
 
 
 precs = {
-    '+': 50,
-    '*': 60,
-    '^': 70
+    'or':  -30,
+    'and': -20,
+    'in': 0, 'is': 0, '<': 0, '<=': 0, '>': 0, '>=': 0, 
+        '<>': 0, '!=': 0, '==': 0, # oops, comparisons are ... different in Python
+    '|': 10,
+    '^': 20,
+    '&': 30,
+    '<<': 40, '>>': 40,
+    '+': 50, '-': 50,
+    '*': 60, '/': 60, '%': 60, '//': 60,
+    '**': 70
 }
+rights = set(['**']) 
 
 def done(stack, last):
     temp = last
@@ -19,11 +26,17 @@ def done(stack, last):
     return temp
 
 def unwind(stack, op2, arg2):
+    """
+    Only handles ties where the operators have identical associativity.
+    Does not handle ties between different associativities.
+    """
     scratch = arg2
     # pop any stack levels with a higher prec than the current operator
     while len(stack) > 0:
         op1, arg1 = stack[-1]
         if precs[op2] > precs[op1]:
+            break
+        if precs[op2] == precs[op1] and op1 in rights:
             break
         stack.pop()
         scratch = node(op1, arg1, scratch)
@@ -50,6 +63,6 @@ def pp(node):
     return node
 
 def run(ys):
-    v = pp(expr([], list(ys)))
+    v = pp(expr([], ys.split(' ')))
     print v
 #    return v
