af913db1c3a631748e251275dbb6fcf2243e21ab
Matt Fenwick <mfenwick100@gmail.com>
2013-08-07 22:15:26 -0400

    js: print AST as indented text in a pre-element b/c it looks better than html lists

--- app/scripts/views/ast.js
+++ app/scripts/views/ast.js
@@ -1,24 +1,36 @@
 define(function() {
+    "use strict";
 
     function AST(elem) {
         this.elem = elem;
     }
     
-    // um, what element is this acting on?
     AST.prototype.display = function(tree) {
-        var str = something(tree);
+        var str = node(tree, 0);
         console.log('lookie: ' + str);
-        this.elem.empty();
-        this.elem.append(str);
+        console.log('huh?  ' + this.elem + ' ' + JSON.stringify(this.elem));
+        this.elem.text(str);
     };
     
-    // need to escape !!!
-    function something(node) {
-        if ( typeof node === 'string' ) {
-            return '<li>' + node + '</li>';
+    // there's got to be a better way to do this
+    function spaces(num) {
+        var arr = new Array(num),
+            i = 0;
+        for(; i < arr.length; i++) {
+            arr[i] = ' ';
         }
-        var op = [node.op.name, node.op.fixity].join(' ');
-        return '<li>' + op + '<ul>' + node.args.map(something).join('') + '</ul></li>';
+        return arr.join(' ');
+    }
+    
+    function node(val, indent) {
+        if ( typeof val === 'string' ) {
+            return spaces(indent * 2) + val;
+        }
+        var op = spaces(indent * 2) + val.op.name + ' [' + val.op.fixity + ']';
+        function f(a) {
+            return node(a, indent + 1);
+        }
+        return [op].concat(val.args.map(f)).join('\n');
     }
     
     return AST;
